da30f7d6d13b46fc53c301b921b28ba63ff568a5
rrabbi7 <rrabbi7@terpmail.umd.edu>
2023-03-30 18:46:26 -0400

    Coded and creates html pages for the favorite course, contact, and base page.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -10,6 +10,22 @@ def hello_world():  # put application's code here
 @app.route('/hello')
 def hello():
     return render_template('hello.html')
+
+@app.route('/favorite-course')
+def favorite_course():
+    print('Subject entered: ' + request.args.get('subject'))
+    print('Course number entered: ' + request.args.get('course_number'))
+
+    return render_template('favorite-course.html')
+
+
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+    if request.method == 'POST':
+        return render_template('contact.html', form_submitted=True)
+    else:
+        return render_template('contact.html')
+
 
 if __name__ == '__main__':
     app.run()
